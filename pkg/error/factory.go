7db110d03c0c6350943cea37678cb6e6ae334951
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2024-11-21 16:09:31 +0100

    Factory docs

--- pkg/error/factory.go
+++ pkg/error/factory.go
@@ -1,3 +1,7 @@
+// Package error provides an implementation of an errors factory for
+// consistent structuring of errors across multiple libraries that
+// invoke logging using this package.
+
 package error
 
 import (
@@ -7,6 +11,8 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// NewFactory Initializes a new error factory with a single
+// IDUnknownError.
 func NewFactory() *Factory {
 	return &Factory{
 		errors: map[ID]*Error{IDUnknownError: &Error{
@@ -19,10 +25,15 @@ func NewFactory() *Factory {
 	}
 }
 
+// Factory object for recording all structured error objects for an
+// application using this module.
 type Factory struct {
 	errors map[ID]*Error
 }
 
+// ExportGOConstants will export constants registered with this module.
+// If no constants have been registered then a single IDUnknown error
+// will be returned in the output requested by the caller.
 func (f *Factory) ExportGOConstants() string {
 	var result = "const (\n"
 	for _, err := range f.errors {
@@ -31,6 +42,8 @@ func (f *Factory) ExportGOConstants() string {
 	return result + ")\n"
 }
 
+// RegisterError provides a way of registering a single correctly
+// formatted errory with the factory object.
 func (f *Factory) RegisterError(id ID, t Type, defaultWeight int64, message string) error {
 	if _, ok := f.errors[id]; ok {
 		return fmt.Errorf("error with id %s already exists", id)
@@ -44,6 +57,8 @@ func (f *Factory) RegisterError(id ID, t Type, defaultWeight int64, message stri
 	return nil
 }
 
+// RegisterErrors records a slice of properly structured errors with
+// the factory object returning nil on success and error on failure.
 func (f *Factory) RegisterErrors(errors []*Error) error {
 	for _, newErr := range errors {
 		if err := f.RegisterError(newErr.ID, newErr.Type, newErr.Weight, newErr.Message); err != nil {
@@ -53,6 +68,10 @@ func (f *Factory) RegisterErrors(errors []*Error) error {
 	return nil
 }
 
+// NewError retrieves an error from the factory. If the error is known
+// its additional context appended and returned to the caller. If the
+// erorr is unknown it is returned with additional context appended and
+// ID unknown.
 func (f *Factory) NewError(id ID, additional string, err error) *Error {
 	archiveErr, ok := f.errors[id]
 	if !ok {
@@ -62,6 +81,8 @@ func (f *Factory) NewError(id ID, additional string, err error) *Error {
 	return archiveErr.WithAdditional(additional, 2, err)
 }
 
+// TOML will return a byte array to the caller containing all
+// registered errors with the exception of IDUnknownError.
 func (f *Factory) TOML() ([]byte, error) {
 	var errs []*Error
 	for _, err := range f.errors {
@@ -73,6 +94,8 @@ func (f *Factory) TOML() ([]byte, error) {
 	return toml.Marshal(_tomlErrors{Errors: errs})
 }
 
+// YAML will return a byte array to the caller containing all
+// registered errors with the exception of IDUnknownError.
 func (f *Factory) YAML() ([]byte, error) {
 	var errs []*Error
 	for _, err := range f.errors {
