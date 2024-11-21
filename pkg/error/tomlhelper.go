5a0d8d593cc68ac830a2a83e8623069d853e6235
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2024-11-21 15:42:53 +0100

    Add YAML and TOML helpers docs

--- pkg/error/tomlhelper.go
+++ pkg/error/tomlhelper.go
@@ -13,6 +13,8 @@ type _tomlErrors struct {
 	Errors []*Error `toml:"errors"`
 }
 
+// LoadTOMLData returns a slice of errors from an input byte array
+// that can be used to to initialize an error factory.
 func LoadTOMLData(data []byte) ([]*Error, error) {
 	var errs = _tomlErrors{Errors: []*Error{}}
 	if _, err := toml.Decode(string(data), &errs); err != nil {
@@ -21,6 +23,8 @@ func LoadTOMLData(data []byte) ([]*Error, error) {
 	return errs.Errors, nil
 }
 
+// LoadTOMLReader returns a slice of errors from an input reade
+// that can be used to to initialize an error factory.
 func LoadTOMLReader(r io.Reader) ([]*Error, error) {
 	var errs = _tomlErrors{Errors: []*Error{}}
 	if _, err := toml.NewDecoder(r).Decode(&errs); err != nil {
@@ -29,6 +33,9 @@ func LoadTOMLReader(r io.Reader) ([]*Error, error) {
 	return errs.Errors, nil
 }
 
+// LoadTOMLFile returns a slice of errors from a filepath pointing at
+// a valid errors toml file. The slice can be used to initialize an
+// error factory.
 func LoadTOMLFile(fp string) ([]*Error, error) {
 	f, err := os.Open(fp)
 	if err != nil {
@@ -38,7 +45,8 @@ func LoadTOMLFile(fp string) ([]*Error, error) {
 	return LoadTOMLReader(f)
 }
 
-// LoadTOMLFileFS loads a toml file from a fs.FS
+// LoadTOMLFile returns a slice of errors from a default filesystem.
+// The slice can be used to initialize an error factory.
 func LoadTOMLFileFS(fSys fs.FS, fp string) ([]*Error, error) {
 	f, err := fSys.Open(fp)
 	if err != nil {
--- pkg/error/yamlhelper.go
+++ pkg/error/yamlhelper.go
@@ -10,6 +10,8 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// LoadYAMLData returns a slice of errors from an input reade
+// that can be used to to initialize an error factory.
 func LoadYAMLData(data []byte) ([]*Error, error) {
 	var errs = []*Error{}
 	if err := yaml.Unmarshal(data, &errs); err != nil {
@@ -18,6 +20,9 @@ func LoadYAMLData(data []byte) ([]*Error, error) {
 	return errs, nil
 }
 
+// LoadYAMLReader returns a slice of errors from a filepath pointing at
+// a valid errors toml file. The slice can be used to initialize an
+// error factory.
 func LoadYAMLReader(r io.Reader) ([]*Error, error) {
 	dec := yaml.NewDecoder(r)
 	var errs = []*Error{}
@@ -27,6 +32,9 @@ func LoadYAMLReader(r io.Reader) ([]*Error, error) {
 	return errs, nil
 }
 
+// LoadYAMLFile returns a slice of errors from a filepath pointing at
+// a valid errors toml file. The slice can be used to initialize an
+// error factory.
 func LoadYAMLFile(fp string) ([]*Error, error) {
 	fp = filepath.Clean(fp)
 	f, err := os.Open(fp)
@@ -37,6 +45,8 @@ func LoadYAMLFile(fp string) ([]*Error, error) {
 	return LoadYAMLReader(f)
 }
 
+// LoadYAMLFileFS returns a slice of errors from a default filesystem.
+// The slice can be used to initialize an error factory.
 func LoadYAMLFileFS(fSys fs.FS, fp string) ([]*Error, error) {
 	fp = filepath.Clean(fp)
 	f, err := fSys.Open(fp)
