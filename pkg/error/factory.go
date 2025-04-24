b2aca94b14a616a330099c9104407a6052571529
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2025-04-24 11:07:23 +0200

    Remove erroneous frame skip
    This was causing logging to report the error module as the source of an
    error instead of the calling module.

--- pkg/error/factory.go
+++ pkg/error/factory.go
@@ -70,14 +70,11 @@ func (f *Factory) RegisterErrors(errors []*Error) error {
 
 // newError is an internal function allowing LogError to wrap NewError
 // and skip the correct stackframe.
-func (f *Factory) newError(id ID, additional string, err error, logExternal bool) *Error {
+func (f *Factory) newError(id ID, additional string, err error) *Error {
 	archiveErr, ok := f.errors[id]
 	if !ok {
 		archiveErr = f.errors[IDUnknownError]
 		additional = string(id) + ": " + additional
-	}
-	if !logExternal {
-		return archiveErr.WithAdditional(additional, runtimeSkipModule, err)
 	}
 	return archiveErr.WithAdditional(additional, runtimeSkipExternalCall, err)
 }
@@ -87,13 +84,13 @@ func (f *Factory) newError(id ID, additional string, err error, logExternal bool
 // error is unknown it is returned with additional context appended and
 // ID unknown.
 func (f *Factory) NewError(id ID, additional string, err error) *Error {
-	return f.newError(id, additional, err, false)
+	return f.newError(id, additional, err)
 }
 
 // LogError provides an simple interface for external callers to return
 // an error from the factory.
 func (f *Factory) LogError(id ID, additional string, err error) (string, *Error) {
-	return f.logName, f.newError(id, additional, err, true)
+	return f.logName, f.newError(id, additional, err)
 }
 
 func (f *Factory) LogSetError(event *zerolog.Event, err error) *zerolog.Event {
